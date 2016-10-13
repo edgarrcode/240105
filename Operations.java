56151bc704b99b44bd7dd703c245659f93d72e7e
Edgar Rodriguez <1983eerr@gmail.com>
2016-10-13 13:21:11 -0600

    first draft

--- Operations.java
+++ Operations.java
@@ -25,12 +25,25 @@ public class Operations {
     provided in the parameter.*/
     static String getRandString(int length) {
     /* Write your code here */
+        String[] letters = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z}
+        String output = "";
+        for (int i = 0; i < length; i++) {
+            //edgarr: check last character "Z", make sure it is used
+            output = output + letters[int(Math.random() * 25)];
+        }
+        return output;
     }
     /* Write a recursive method to print all the Strings separated by lines.
     This method cannot contain any loop (that is, uses of for, while, do while
     are prohibited). */
     public static void printMyList(StringNode M){
     /* Write your code here */
+        if (M == null) {
+            return;
+        }
+        System.out.println(M.head);
+        printMyList(M.next);
+
     }
     /* Write a recursive method to retrieve the number of strings that have
     exactly the same length provided in the parameter. This method cannot
@@ -38,6 +51,14 @@ public class Operations {
     */
     public static int countKLenghthStrings (StringNode M, int k){
     /* Write your code here */
+        if (M == null) {
+            return 0;
+        }
+        int counter = 0;
+        if (M.head.length() == k) {
+            counter++;
+        }
+        return counter  + countKLenghthStrings(M.next);
     }
     /* Write a recursive method to retrieve the longest String from the list.
     Assume that there is at least one String in the given list when the method
@@ -45,6 +66,19 @@ public class Operations {
     is, uses of for, while, do while are prohibited).*/
     public static String longestStringOfMyList (StringNode M){
     /* Write your code here */
+        if (M == null) {
+            return "Your list is empty!";
+        }
+        if (M.next == null) {
+            return M.head;
+        }
+
+        String theLongestString = longestStringOfMyList(M.next);
+        //edgarr:check limits
+        if (M.head.length() > theLongestString) {
+            return M.head;
+        }
+        return theLongestString;
     }
     /* Write a recursive method to compute the length of a list.
     This method cannot contain any loop (that is, uses of for, while, do while
@@ -52,6 +86,10 @@ public class Operations {
     */
     public static int lengthOfMyList (StringNode M){
     /* Write your code here */
+        if (M == null) {
+            return 0;
+        }
+        return 1 + lengthOfMyList(M.next);
     }
     /* Write a recursive method named reverseMyList that will reverse a given
     linked list M. Return the head of the reversed linked list. It is fine
@@ -59,5 +97,6 @@ public class Operations {
     */
     public static StringNode reverseMyList (StringNode M){
     /* Write your code here */
+        
     }
 }
